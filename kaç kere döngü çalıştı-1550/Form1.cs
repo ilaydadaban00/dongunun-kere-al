55308065cbfd7c5cfba4958c9f2bf949a88ea93a
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-27 04:14:19 +0300

    Add files via upload

---
+++ kaç kere döngü çalıştı-1550/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kaç_kere_döngü_çalıştı_1550
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dongu_say = 1;
+            int toplam = 0;
+            while (toplam <= 1000)
+            {
+                toplam += dongu_say;
+                dongu_say+= 1;
+            }
+            MessageBox.Show("döngü toplam" + dongu_say.ToString() + "kez çalıştı");
+        }
+    }
+}
